9c2ec510b2b20c73c7947e71171f7b92498fa04a
DeArvis <dearvistroutman@gmail.com>
2020-08-01 15:10:50 -0400

    First commit

---
+++ src/main/java/com/techninja/salesmanager/SalesDAO.java
@@ -0,0 +1,40 @@
+package com.techninja.salesmanager;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+  
+@Repository
+@Transactional
+public class SalesDAO
+{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+  
+    public void save(Sale sale) {
+    }
+  
+    public Sale get(int id) {
+        return null;
+    }
+  
+    public void update(Sale sale) {
+    }
+  
+    public void delete(int id) {
+    }
+    
+    public List<Sale> list() {
+        String sql = "SELECT * FROM SALES";
+     
+        List<Sale> listSale = jdbcTemplate.query(sql,
+                BeanPropertyRowMapper.newInstance(Sale.class));
+     
+        return listSale;
+    }
+}
+
